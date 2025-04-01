2de2dfc452b7d5b8d673b93ecaf717f38da85918
Jacob Edsell <jacobedsell@gmail.com>
2025-04-01 11:24:48 -0700

    running 1 thread per physical thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use tokio;
+use std::thread;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
+    let num_threads = thread::available_parallelism()
+        .map(|n| n.get())
+        .unwrap_or(4);
+    
+    println!("Running with {} threads", num_threads);
+    
     let mut handles = vec![];
 
-    // Spawn 20 tasks
-    for i in 1..=20 {
+    // Spawn tasks equal to number of physical threads
+    for i in 1..=num_threads {
         let handle = tokio::spawn(async move {
             println!("Thread {} is running", i);
         });
